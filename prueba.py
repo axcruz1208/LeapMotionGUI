a649e114e265043a77b6fd90aa0231ecc5995d90
Alex Cruz <alexcruzlemos@correo.ugr.es>
2021-11-08 17:47:01 +0100

    Añadido la elección cuando vas a salir de la aplicación, cursor de gestos, gesto para volver del frame 2

--- prueba.py
+++ prueba.py
@@ -1,35 +1,48 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QGridLayout, \
-    QScrollArea, QDialog, QStackedWidget
-from PyQt5.QtGui import QPixmap, QCursor, QMouseEvent
-from PyQt5 import QtCore, QtTest
-import Leap, _thread, time
-from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
 import threading
-import mouse
+import time
+
+from PyQt5 import QtCore
+from PyQt5.QtGui import QPixmap, QCursor, QFont
+from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QGridLayout, \
+    QScrollArea, QDialog, QStackedWidget, QFrame, QSizePolicy
+
+import Leap
+
+class QHSeperationLine(QFrame):
+  '''
+  a horizontal seperation line\n
+  '''
+  def __init__(self):
+    super().__init__()
+    self.setMinimumWidth(1)
+    self.setFixedHeight(5)
+    self.setStyleSheet("background: '#ECE5EC';")
+    self.setFrameShape(QFrame.HLine)
+    self.setFrameShadow(QFrame.Sunken)
+    self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Minimum)
+    return
 
 """
 Clase que nos crea una especie de tarjeta para almacenar las clases/tutorias/conferencia que vamos a tener
 con una fotografía e información sobre las mismas
 """
 class CardView(QWidget):
-    def __init__(self, image,name, information, *args, **kwargs):
+    def __init__(self, image,name, information, size, *args, **kwargs):
         super(CardView, self).__init__(*args, **kwargs)
 
         #Imagen con sus propiedades
         lessonImage = QPixmap(image)
-        lessonImage = lessonImage.scaled(290, 290)
+        lessonImage = lessonImage.scaled(size, size)
         cardViewImage = QLabel()
         cardViewImage.setPixmap(lessonImage)
-        cardViewImage.setStyleSheet("margin-left: 400px;")
 
         #Información sobre la clase en cuestión
         name = QLabel(name)
-        name.setAlignment(QtCore.Qt.AlignLeft)
-        name.setStyleSheet(	"font-size: 50px;" + "color: 'white';" + "margin-right: 150px")
+        name.setFont(QFont('Times', size/10))
+        name.setStyleSheet("color: '#ECE5EC';" + "margin-right: 150px;")
         information = QLabel(information)
-        information.setAlignment(QtCore.Qt.AlignTop)
-        information.setStyleSheet(	"font-size: 25px;")
+        information.setFont(QFont('Times', size/20))
 
         #Layout vertical que almacena la información
         verticalLayout = QVBoxLayout()
@@ -38,6 +51,7 @@ class CardView(QWidget):
 
         #Layout horizontal que almacena imagen y layout vertical
         layout = QHBoxLayout()
+        layout.setAlignment(QtCore.Qt.AlignCenter)
         layout.addWidget(cardViewImage)
         layout.addLayout(verticalLayout)
 
@@ -92,7 +106,7 @@ class MainFrame(QDialog):
         )
 
         #Botón de Brújula
-        button3 = QPushButton("EXIT")
+        button3 = QPushButton("Salir")
         button3.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
         button3.setStyleSheet(
             "*{border: 4px solid '#BC006C';" +
@@ -118,46 +132,48 @@ class MainFrame(QDialog):
     Función que nos redirige a al frame del horario
     """
     def gotoScheduleFrame(self):
-        window.setCurrentIndex(window.currentIndex()+1)
+        window.setCurrentIndex(window.currentIndex()+2)
 
 """
 Clase que crea el frame del horario que nos muestra eventos cercanos de relevancia (clases, tutorías, etc)
 """
-class ScheduleFrame(QDialog):
+class ElectionFrame(QDialog):
     def __init__(self):
-        super(ScheduleFrame, self).__init__()
+        super(ElectionFrame, self).__init__()
 
         #ScheduleFrame Widgets/layouts
-        scrollArea = QScrollArea()
-        widget = QWidget()
         verticalLayout = QVBoxLayout()
-        layout = QVBoxLayout()
+        horizontalLayout = QHBoxLayout()
+        separator_horizontal = QHSeperationLine()
+        separator_horizontal2 = QHSeperationLine()
 
-        #Creamos los distintos eventos del horario
-        lesson1 = CardView("VC.png", "Visión por Computador (Teoría)", "Aula: 1.7\nHora: 8:30-10:30\nProfesor: Nicolás Pérez de la Blanca Capilla")
-        lesson2 = CardView("VC.png", "Visión por Computador (Practicas)", "Aula: 3.7\nHora: 10:30-12:30\nProfesor: Nicolás Pérez de la Blanca Capilla")
-        lesson3 = CardView("RSC.png", "Procesadores de Lenguajes (Practicas)", "Aula: 0.8\nHora: 12:30-14:30\nProfesor: Ramón López-Cózar Delgado")
-        lesson4 = CardView("tutoria.png", "Tutoría (Visión por Computador)", "Aula: 1.7\nHora: 13:30-17:30\nProfesor: Nicolás Pérez de la Blanca Capilla")
-        lesson5 = CardView("conference.png", "Conferencia (Ciberseguridad)", "Aula: 0.3\nHora: 19:30-20:30\nPonente: Patricia Díez Muñoz")
+        #Obtenemos la imagen de Like
+        image = QPixmap("Like.png")
+        like = QLabel()
+        like.setPixmap(image)
+        like.setAlignment(QtCore.Qt.AlignCenter)
+        like.setStyleSheet("margin-top: 25px;" + "margin-bottom: 25px;")
 
-        #Añadimos dichos eventos al layout
-        layout.addWidget(lesson1)
-        layout.addWidget(lesson2)
-        layout.addWidget(lesson3)
-        layout.addWidget(lesson4)
-        layout.addWidget(lesson5)
+        #Obtenemos la imagen de Stop
+        image = QPixmap("Stop.png")
+        stop = QLabel()
+        stop.setPixmap(image)
+        stop.setAlignment(QtCore.Qt.AlignCenter)
+        stop.setStyleSheet("margin-top: 25px;" + "margin-bottom: 25px;")
 
-        #Vinculamos el layout con otro widget (QScrollArea necesita un widget para funcionar)
-        widget.setLayout(layout)
+        #Formulamos la pregunta y la añadimos al layout
+        question = QLabel("¿Seguro que quieres salir?")
+        question.setAlignment(QtCore.Qt.AlignCenter)
+        question.setFont(QFont('Times', 50))
+        question.setStyleSheet("color: '#ECE5EC';")
+        verticalLayout.addWidget(question)
+        verticalLayout.addWidget(separator_horizontal)
 
-        #Añadimos propiedades al scrollArea y el widget creado antes
-        scrollArea.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
-        scrollArea.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        scrollArea.setWidgetResizable(True)
-        scrollArea.setWidget(widget)
-
-        #Añadimos el scrollArea al layout principal
-        verticalLayout.addWidget(scrollArea)
+        #Añadimos las fotos al layout horizontal y lo añadimos al vergtical
+        horizontalLayout.addWidget(stop)
+        horizontalLayout.addWidget(like)
+        verticalLayout.addLayout(horizontalLayout)
+        verticalLayout.addWidget(separator_horizontal2)
 
         #Motramos el logo de nuevo más pequeño
         image = QPixmap("logo.png")
@@ -171,6 +187,69 @@ class ScheduleFrame(QDialog):
 
         #Enlazamos el layout principal con la clase QDialog
         self.setLayout(verticalLayout)
+
+"""
+Clase que crea el frame del horario que nos muestra eventos cercanos de relevancia (clases, tutorías, etc)
+"""
+class ScheduleFrame(QDialog):
+    def __init__(self):
+        super(ScheduleFrame, self).__init__()
+
+        #ScheduleFrame Widgets/layouts
+        self.scrollArea = QGridLayout()
+        self.scrollArea.setAlignment(QtCore.Qt.AlignCenter)
+        verticalLayout = QVBoxLayout()
+        separator_horizontal = QHSeperationLine()
+        separator_horizontal2 = QHSeperationLine()
+
+        image = QPixmap("upArrow.png")
+        UA = QLabel()
+        UA.setPixmap(image)
+        UA.setAlignment(QtCore.Qt.AlignCenter)
+
+        image = QPixmap("downArrow.png")
+        DA = QLabel()
+        DA.setPixmap(image)
+        DA.setAlignment(QtCore.Qt.AlignCenter)
+
+        #Creamos los distintos eventos del horario
+        empty = CardView("empty.png", "", "", 175)
+        lesson1 = CardView("VC.png", "Visión por Computador (Teoría)", "Aula: 1.7\nHora: 8:30-10:30\nProfesor: Nicolás Pérez de la Blanca Capilla", 350)
+        lesson2 = CardView("VC.png", "Visión por Computador (Practicas)", "Aula: 3.7\nHora: 10:30-12:30\nProfesor: Nicolás Pérez de la Blanca Capilla", 175)
+        lesson3 = CardView("RSC.png", "Procesadores de Lenguajes (Practicas)", "Aula: 0.8\nHora: 12:30-14:30\nProfesor: Ramón López-Cózar Delgado", 175)
+        lesson4 = CardView("tutoria.png", "Tutoría (Visión por Computador)", "Aula: 1.7\nHora: 13:30-17:30\nProfesor: Nicolás Pérez de la Blanca Capilla", 100)
+
+        #Añadimos dichos eventos al layout
+        self.scrollArea.addWidget(UA, 0,0)
+        self.scrollArea.addWidget(empty,1,0)
+        self.scrollArea.addWidget(separator_horizontal,2,0)
+        self.scrollArea.addWidget(lesson1,3,0)
+        self.scrollArea.addWidget(separator_horizontal2,4,0)
+        self.scrollArea.addWidget(lesson3,5,0)
+        self.scrollArea.addWidget(DA,6,0)
+
+        #Añadimos el scrollArea al layout principal
+        verticalLayout.addLayout(self.scrollArea)
+
+        #Motramos el logo de nuevo más pequeño
+        image = QPixmap("logo.png")
+        logo = QLabel()
+        logo.setPixmap(image)
+        logo.setAlignment(QtCore.Qt.AlignCenter)
+        logo.setStyleSheet("margin-top: 20px;")
+
+        #Añadimos el logo al layout principal
+        verticalLayout.addWidget(logo)
+
+        #Enlazamos el layout principal con la clase QDialog
+        self.setLayout(verticalLayout)
+
+    def deleteActualLesson(self):
+        self.scrollArea.itemAt(3).widget().deleteLater()
+
+    def addActualLesson(self):
+        lesson5 = CardView("conference.png", "Conferencia (Ciberseguridad)","Aula: 0.3\nHora: 19:30-20:30\nPonente: Patricia Díez Muñoz", 350)
+        self.scrollArea.addWidget(lesson5,2,0)
 
 class SampleListener(Leap.Listener):
     finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
@@ -198,6 +277,8 @@ class SampleListener(Leap.Listener):
 
     def on_frame(self, controller):
         # Get the most recent frame and report some basic information
+        global is_gesturing
+        global schedule
         frame = controller.frame()
 
         print("Frame id: %d, timestamp: %d, hands: %d, fingers: %d, tools: %d, gestures: %d" % (
@@ -209,6 +290,16 @@ class SampleListener(Leap.Listener):
             handType = "Left hand" if hand.is_left else "Right hand"
             print ("  %s, id %d, position: %s" % (handType, hand.id, hand.palm_position))
 
+            # Get the hand's normal vector and direction
+            normal = hand.palm_normal
+            direction = hand.direction
+
+            # Calculate the hand's pitch, roll, and yaw angles
+            print ("  pitch: %f degrees, roll: %f degrees, yaw: %f degrees" % (
+                direction.pitch * Leap.RAD_TO_DEG,
+                normal.roll * Leap.RAD_TO_DEG,
+                direction.yaw * Leap.RAD_TO_DEG))
+
             #Obtenemos los distintos dedos de la mano
             thumb = hand.fingers[0]
             index = hand.fingers[1]
@@ -216,15 +307,51 @@ class SampleListener(Leap.Listener):
             ring = hand.fingers[3]
             little = hand.fingers[4]
 
-            x_position = int(((index.tip_position.x + 175) / 350) * 1920)
-            y_position = (int(((index.tip_position.y-100) / 200) * 1080)-1080)*-1
+            x_finger_position = int(((index.tip_position.x + 175) / 350) * 1920)
+            y_finger_position = (int(((index.tip_position.y-100) / 200) * 1080)-1080)*-1
 
+            x_hand_position = int(((hand.palm_position.x + 175) / 350) * 1920)
+            y_hand_position = (int(((hand.palm_position.y-100) / 200) * 1080)-1080)*-1
+
+            #Si solo tenemos sacado el dedo indice podemos controllar el ratón y pulsar botones
             if index.is_extended and not(middle.is_extended) and not(thumb.is_extended) and not(ring.is_extended) and not(little.is_extended):
-                current_cursor.setPos(x_position, y_position)
+                current_cursor.setPos(x_finger_position, y_finger_position)
                 app.setOverrideCursor(current_cursor)
+                is_gesturing = False
 
-                if (index.tip_velocity[2] < -400 and x_position > 114 and x_position < 849 and y_position > 583 and y_position < 724):
+                if (index.tip_velocity[2] < -400 and window.currentIndex() == 0 and x_finger_position > 114 and x_finger_position < 849 and y_finger_position > 583 and y_finger_position < 724):
+                    window.setCurrentIndex(window.currentIndex()+2)
+
+                if (index.tip_velocity[2] < -400 and window.currentIndex() == 0 and x_finger_position > 114 and x_finger_position < 1806 and y_finger_position > 839 and y_finger_position < 976):
                     window.setCurrentIndex(window.currentIndex()+1)
+
+            # Si extendemos todos los dedos podemos realizar gestos
+            if index.is_extended and middle.is_extended and thumb.is_extended and ring.is_extended and little.is_extended and not(window.currentIndex() == 1):
+                current_cursor.setPos(x_hand_position, y_hand_position)
+                app.setOverrideCursor(gesture_cursor)
+                is_gesturing = True
+
+                #Swipe
+                if (hand.palm_velocity[1] < -400):
+                    schedule.deleteActualLesson()
+                    print("SWIPE ABAJO\nSWIPE ABAJO\nSWIPE ABAJO\nSWIPE ABAJO\nSWIPE ABAJO\nSWIPE ABAJO\nSWIPE ABAJO\nSWIPE ABAJO\n")
+                elif(hand.palm_velocity[1] > 400):
+                    schedule.addActualLesson()
+                    print("SWIPE ARRIBA\nSWIPE ARRIBA\nSWIPE ARRIBA\nSWIPE ARRIBA\nSWIPE ARRIBA\nSWIPE ARRIBA\nSWIPE ARRIBA\n")
+
+            #Gesto para volver a la pestaña anterior
+            if(is_gesturing and window.currentIndex() == 2 and not(index.is_extended) and not(middle.is_extended) and not(thumb.is_extended) and not(ring.is_extended) and not(little.is_extended)):
+                window.setCurrentIndex(window.currentIndex()-2)
+
+            if(window.currentIndex() == 1):
+                if (handType == "Right hand"):
+                    if (normal.roll * Leap.RAD_TO_DEG > -120 and normal.roll * Leap.RAD_TO_DEG < -70):
+                        if not (index.is_extended) and not (middle.is_extended) and not (ring.is_extended) and not (little.is_extended):
+                            app.quit()
+                            sys.exit()
+                    if (direction.pitch * Leap.RAD_TO_DEG > 40 and direction.pitch * Leap.RAD_TO_DEG < 70):
+                        if index.is_extended and middle.is_extended and thumb.is_extended and ring.is_extended and little.is_extended:
+                            window.setCurrentIndex(window.currentIndex()-1)
 
         if not (frame.hands.is_empty and frame.gestures().is_empty):
             print ("")
@@ -248,8 +375,9 @@ def main():
         controller.remove_listener(listener)
 
 
-#Creamos la aplicación
+#Creamos la aplicación y variables globales
 app = QApplication(sys.argv)
+is_gesturing = False
 
 #Ponemos a ejecutar el listener en segundo plano ( en la terminal )
 download_thread = threading.Thread(target=main, name="MainFunction")
@@ -257,7 +385,8 @@ download_thread.start()
 
 #Definimos los dintintos frames de la app
 mainWindow = MainFrame()
-horario = ScheduleFrame()
+election = ElectionFrame()
+schedule = ScheduleFrame()
 
 #Creamos la ventana
 window = QStackedWidget()
@@ -272,9 +401,15 @@ cursor_pix = QPixmap('hand.png')
 cursor_scaled_pix = cursor_pix.scaled(QtCore.QSize(60, 60), QtCore.Qt.KeepAspectRatio)
 current_cursor = QCursor(cursor_scaled_pix, -1, -1)
 
+#Cursor para los gestos
+cursor_pix = QPixmap('gesture.png')
+cursor_scaled_pix = cursor_pix.scaled(QtCore.QSize(60, 60), QtCore.Qt.KeepAspectRatio)
+gesture_cursor = QCursor(cursor_scaled_pix, -1, -1)
+
 #Almacenamos los frames en la ventana y mostramos por pantalla
 window.addWidget(mainWindow)
-window.addWidget(horario)
+window.addWidget(election)
+window.addWidget(schedule)
 window.show()
 
 sys.exit(app.exec())
